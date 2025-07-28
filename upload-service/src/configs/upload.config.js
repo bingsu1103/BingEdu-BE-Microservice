9259d7899d8cee0d71bac2691a6b4ff0194a5d7c
An Ngo <ngogiaan11111@gmail.com>
2025-07-28 17:04:56 +0700

    commit model user

--- upload-service/src/configs/upload.config.js
+++ upload-service/src/configs/upload.config.js
@@ -3,27 +3,25 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET_KEY
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: "bingeduimg/courses/thumbnail",
-        allowed_formats: ["jpg", "jpeg", "png", "gif", "webp", "avif"],
-        transformation: [{ width: 1000, height: 1000, crop: "limit" }]
-    }
+  cloudinary: cloudinary,
+  params: {
+    folder: "bingeduimg/courses/thumbnail",
+    allowed_formats: ["jpg", "jpeg", "png", "gif", "webp", "avif"],
+    transformation: [{ width: 600, height: 600, crop: "limit" }],
+  },
 });
 
-const upload = multer(
-    {
-        storage: storage,
-        limits: {
-            fileSize: 1024 * 1024 * 2
-        }
-    }
-);
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 2,
+  },
+});
 
 module.exports = upload;
--- user-service/src/models/user.js
+++ user-service/src/models/user.js
@@ -2,59 +2,64 @@ const mongoose = require("mongoose");
 const mongoose_delete = require("mongoose-delete");
 const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        phone: {
-            type: String,
-            required: true
-        },
-        role: {
-            type: String,
-            enum: ["user", "admin"],
-            required: true,
-            default: "user"
-        },
-        type: {
-            type: String,
-            enum: ["normal", "vip"],
-            required: true,
-            default: "normal"
-        },
-        is_active: {
-            type: Boolean,
-            default: true
-        },
-        avatar: String,
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
-)
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      required: true,
+      default: "user",
+    },
+    avatar: {
+      type: String,
+      default: "https://github.com/shadcn.png",
+    },
+    location: String,
+    type: {
+      type: String,
+      enum: ["normal", "vip"],
+      required: true,
+      default: "normal",
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    avatar: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        return next();
-    }
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        await this.constructor.collection.dropIndexes();
-        next();
-    } catch (error) {
-        next(error);
-    }
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    await this.constructor.collection.dropIndexes();
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-userSchema.plugin(mongoose_delete, { overrideMethods: 'all' });
-const User = mongoose.model('User', userSchema);
+userSchema.plugin(mongoose_delete, { overrideMethods: "all" });
+const User = mongoose.model("User", userSchema);
 module.exports = User;
