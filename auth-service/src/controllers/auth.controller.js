1014161014914c23b08ef4c2916e72c36283a7cf
An Ngo <ngogiaan11111@gmail.com>
2025-08-09 21:14:35 +0700

    test

--- auth-service/src/controllers/auth.controller.js
+++ auth-service/src/controllers/auth.controller.js
@@ -50,7 +50,7 @@ const loginAPI = async (req, res) => {
       return res.status(200).json({
         status: false,
         EC: result.EC || 2,
-        message: result.message || "Unauthorized",
+        message: result || "Unauthorized",
         data: null,
       });
     }
