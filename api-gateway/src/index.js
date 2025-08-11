5b76d40ee51f83ac3159988ab475aeb146c51927
An Ngo <ngogiaan11111@gmail.com>
2025-08-11 15:28:43 +0700

    cors dev

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -20,7 +20,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://bingedu.vercel.app",
+    origin:
+      process.env.FRONTEND_URL ||
+      "https://bingedu.vercel.app" ||
+      "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
